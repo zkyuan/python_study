cb55930c5c9d6bfec0a146be4fb6f64e3a526803
zkyuan <2584278161@qq.com>
2024-12-16 22:28:23 +0800

    mf

---
+++ py1/py01.py
@@ -0,0 +1,28 @@
+# 运算符
+# 除 / ，结果是浮点数float
+print(1 / 1)
+# 除取整 //
+print(3 // 2)
+# 除取余数 %
+print(8 % 3)
+# 幂 ** ，m**n：m的n次方
+print(2 ** 3)
+# 算数运算符有浮点数，计算结果为浮点数
+print(2.0 * 3)
+print(2.0 + 3)
+
+# 赋值运算符 =
+num1 = 5
+num2 = num1
+print(num2)
+num3 = num1 + num2
+print(num3)
+num3 += 3
+print(num3)
+
+# 输入函数 input，  参数prompt：输入时的提示
+s = input("请输入")
+print(s)
+# 输入类型控制 int float
+i = int(input("请输入数字"))
+print(i)
---
+++ py1/py02.py
@@ -0,0 +1,31 @@
+# if判断 冒号一定要有
+# 代码格式整理：ctrl+alt+L
+a = 1
+if a > 0:
+    print("true")
+else:
+    print("false")
+
+# 比较运算符 == ！= > < >= <=
+# 逻辑运算符 and or not
+# 三目运算符 ：为真结果 if 判断条件 else 为假结果
+print("true") if a > 0 else print("fales")
+
+# score 成绩90-100优秀，60-90合格，0-60不合格
+# score = int(input("请输入成绩"))
+score = 900
+flag = 1
+while flag == 1:
+    score = int(input("请输入成绩"))
+    if score >= 90 and score <= 100:  # 可以这样写 90 <= score <= 100
+        print("成绩优秀")
+        flag = 0
+    elif score >= 60 and score < 90:
+        print("成绩合格")
+        flag = 0
+    elif score >= 0 and score < 60:
+        print("成绩不合格")
+        flag = 0
+    else:
+        print("成绩输入错误，请重新输入")
+        flag = 1
