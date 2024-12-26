479d60836dd1bd1e9f78a2332a544bb35d88d9be
zkyuan <2584278161@qq.com>
2024-12-26 15:26:07 +0800

    add

--- py1/py08.py
+++ py1/py08.py
@@ -47,3 +47,60 @@ def fun4(**kwargs):
 fun4(name="zky", age="18")
 
 # 函数嵌套：在一个函数里面调用另一个函数
+# 嵌套定义
+
+# 作用域  全局变量、局部变量
+# 申请为全局变量 global
+# nonlocal 申明外一层的局部变量，用此申明，能对上一层的变量进行修改
+a = 100
+
+
+def fun5(*args):
+    print("fun5函数体：")
+    a = 110
+    b = 200
+    global c  # 全局变量先申明，再赋值
+    c = 300
+    print(a)
+
+
+fun5()  # 110
+print(a)  # 100
+# print(b) # 报错没有b变量
+print(c)  # 300
+c = 111
+print(c)  # 111
+
+
+# 匿名函数：函数名 = lambda 形参 : 返回值
+# 普通函数
+def add(a, b):
+    return a + b
+
+
+print(add(1, 2))
+# 匿名函数
+add = lambda a, b: a + b
+print(add(1, 3))
+
+# 没有参数
+fun6 = lambda: "fun1：无参的匿名函数"
+print(fun6())
+
+# 默认参数(默认参数必须写在非默认参数后面)
+fun7 = lambda name, age=18: (name, age)
+print(fun7("zky", 20))
+print(fun7("zky"))
+
+# 关键字参数
+fun8 = lambda **kwargs: kwargs
+print(fun8(name="zhangkyuan", age="22"))
+
+# lambda与if结合
+
+# 内置函数
+# 查看所有内置函数
+import builtins
+
+print(dir(builtins))  # 大写开头：内置常量名 ； 小写开头：内置函数名
+
