66906cc5d9e85db97e4605ed653a84342211fcf6
zkyuan <2584278161@qq.com>
2025-02-21 15:37:40 +0800

    add

---
+++ py5/demo.py
@@ -0,0 +1,31 @@
+
+import requests
+
+
+def request_chatgpt_function():
+    url = "https://api.aihao123.cn/luomacode-api/open-api/v1/chat/completions"  # 可以替换为任何代理的接口
+    OPENAI_API_KEY = "sk-4bip3gmlp5dd3qiu9qc1uirjldv73n6o9j82vv309lvo8r3j"  # openai官网获取key
+    header = {"Content-Type": "application/json", "Authorization": "Bearer " + OPENAI_API_KEY}
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+            {
+                "role": "system",
+                "content": "You are a helpful assistant."
+            },
+            {
+                "role": "user",
+                "content": "Hello! ZhouMin is my college roommate, how is his financial luck this year? can you tell me？"
+            }
+        ],
+        "temperature": 0,
+        "stream": False
+    }
+    response = requests.post(url=url, headers=header, json=data).json()
+    # print(response)
+    print(response)
+    return response
+
+
+if __name__ == "__main__":
+    request_chatgpt_function()  # 利用openai正常调用
