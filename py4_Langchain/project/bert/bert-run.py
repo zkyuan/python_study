f26b48c6d8511958adae34d025e8ed5000cf917b
zkyuan <2584278161@qq.com>
2025-03-01 21:20:42 +0800

    add

--- py4_Langchain/project/bert/bert-run.py
+++ py4_Langchain/project/bert/bert-run.py
@@ -1,9 +1,9 @@
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
 
 # 设置具体包含 config.json 的目录
-#官方模型
-#model_dir = r"D:\software\huggingface\hub\models--bert-base-chinese\snapshots\c30a6ed22ab4564dc1e3b2ecbf6e766b0611a33f"  # 替换为实际路径
-#自己训练的模型
+# 官方模型
+# model_dir = r"C:\Users\HP\.cache\huggingface\hub\models--bert-base-chinese\snapshots\c30a6ed22ab4564dc1e3b2ecbf6e766b0611a33f"  # 替换为实际路径
+# 自己训练的模型
 model_dir = r"./sentiment_model"
 
 # 加载模型和分词器
@@ -11,21 +11,33 @@ model = AutoModelForSequenceClassification.from_pretrained(model_dir)
 tokenizer = AutoTokenizer.from_pretrained(model_dir)
 
 # 使用加载的模型和分词器创建分类任务的 pipeline
-classifier = pipeline("text-classification", model=model, tokenizer=tokenizer,device="cpu")
+classifier = pipeline("text-classification", model=model, tokenizer=tokenizer, device="cpu")
 
 # 执行分类任务
-#正向情绪分类
+# 正向情绪分类
 #
-#LABEL_0：在二分类情感分析任务中，0 通常表示“负面”情感。
-#LABEL_1：相应地，1 通常表示“正面”情感。
+# LABEL_0：在二分类情感分析任务中，0 通常表示“负面”情感。
+# LABEL_1：相应地，1 通常表示“正面”情感。
 output = classifier("我今天心情很好")
 print(output)
-#[{'label': 'LABEL_1', 'score': 0.5915976762771606}]
+# [{'label': 'LABEL_1', 'score': 0.9958015084266663}]
 
-output = classifier("你好，我是AI助手")
+output = classifier("你好，我是福州第一温柔")
 print(output)
-#[{'label': 'LABEL_1', 'score': 0.5109264254570007}]
+# [{'label': 'LABEL_0', 'score': 0.9950175881385803}]
 
 output = classifier("我今天很生气")
 print(output)
-#[{'label': 'LABEL_1', 'score': 0.6152875423431396}]
+# [{'label': 'LABEL_0', 'score': 0.9962648749351501}]
+
+output = classifier("你好坏哦，我好喜欢")
+print(output)
+# [{'label': 'LABEL_1', 'score': 0.9936390519142151}]
+
+output = classifier("你好坏")
+print(output)
+# [{'label': 'LABEL_0', 'score': 0.9965997338294983}]
+
+output = classifier("你好帅")
+print(output)
+# [{'label': 'LABEL_1', 'score': 0.9938560128211975}]
--- py4_Langchain/project/bert/bert-trainer.py
+++ py4_Langchain/project/bert/bert-trainer.py
@@ -1,15 +1,15 @@
-# 步骤 1：环境准备
+# 1：环境准备
 # pip install torch transformers datasets scikit-learn
 from transformers import BertTokenizer, BertForSequenceClassification
 
-# 步骤 2：加载中文 BERT 预训练模型
-# 加载 BERT 中文预训练模型和分词器
-#可以自定义huggingface模型下载的位置
-#setx HF_HOME “D:\software\huggingface“  默认位置c:\\users\用户名\.cache\huggingface
+# 2：加载中文 BERT 预训练模型
+# 加载 bert 中文预训练模型和分词器
+# 可以自定义huggingface模型下载的位置
+# setx HF_HOME “D:\huggingface“  默认位置C:\Users\HP\.cache\huggingface
 tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
 model = BertForSequenceClassification.from_pretrained('bert-base-chinese', num_labels=3)
 
-# 步骤 3：加载 ChnSentiCorp 数据集并进行清洗
+# 3：加载 ChnSentiCorp 数据集并进行清洗
 from datasets import load_dataset
 
 # 加载 ChnSentiCorp 数据集
@@ -17,10 +17,14 @@ from datasets import load_dataset
 dataset = load_dataset('lansinuote/ChnSentiCorp')
 
 import re
+
+
 # 定义数据清洗函数
 def clean_text(text):
-    text = re.sub(r'[^\w\s]', '', text)  # 去除标点符号
-    text = text.strip()  # 去除前后空格
+    # 去除标点符号
+    text = re.sub(r'[^\w\s]', '', text)
+    # 去除前后空格
+    text = text.strip()
     return text
 
 
@@ -28,7 +32,7 @@ def clean_text(text):
 dataset = dataset.map(lambda x: {'text': clean_text(x['text'])})
 
 
-# 步骤 4：数据预处理
+# 4：数据预处理
 def tokenize_function(examples):
     return tokenizer(examples['text'], padding='max_length', truncation=True, max_length=128)
 
@@ -36,31 +40,39 @@ def tokenize_function(examples):
 # 对数据集进行分词和编码
 encoded_dataset = dataset.map(tokenize_function, batched=True)
 
-# 步骤 5：训练模型
+# 5：训练模型
 from transformers import Trainer, TrainingArguments
 
 # 定义训练参数
 # 定义训练参数，创建一个TrainingArguments对象
 training_args = TrainingArguments(
-    output_dir='./results',  # 指定训练输出的目录，用于保存模型和其他输出文件
-    num_train_epochs=1,  # 设置训练的轮数，这里设置为1轮
-    per_device_train_batch_size=1,  # 每个设备（如GPU）上的训练批次大小，这里设置为1
-    per_device_eval_batch_size=1,  # 每个设备上的评估批次大小，这里设置为1
-    evaluation_strategy="epoch",  # 设置评估策略为每个epoch结束后进行评估
-    logging_dir='./logs',  # 指定日志保存的目录，用于记录训练过程中的日志信息
+    # 指定训练输出的目录，用于保存模型和其他输出文件
+    output_dir='./results',
+    # 设置训练的轮数
+    num_train_epochs=1,
+    # 每个设备（如GPU）上的训练批次大小
+    per_device_train_batch_size=1,
+    # 每个设备上的评估批次大小
+    per_device_eval_batch_size=1,
+    # 设置评估策略为每个epoch结束后进行评估
+    evaluation_strategy="epoch",
+    # 指定日志保存的目录
+    logging_dir="./logs",
 )
 
-# 使用 Trainer 进行训练
+print("---------------开始训练---------------")
+# 使用 Trainer 进行训练 ,Trainer 是一个简单但功能齐全的 PyTorch 训练和评估循环
 trainer = Trainer(
     model=model,
     args=training_args,
+    # 训练集
     train_dataset=encoded_dataset['train'],
+    # 评估集
     eval_dataset=encoded_dataset['validation'],
 )
 
 # 开始训练
 trainer.train()
-#{'loss': 0.7493, 'grad_norm': 31.590713500976562, 'learning_rate': 1.3541666666666666e-05, 'epoch': 0.73}
 
 # 步骤 6：评估模型性能
 from sklearn.metrics import accuracy_score
@@ -68,22 +80,24 @@ from sklearn.metrics import accuracy_score
 
 # 定义评估函数
 def compute_metrics(p):
-    preds = p.predictions.argmax(-1)
+    # p.predictions 是模型对输入数据的预测输出，
+    preds = p.predictions.argmax(-1)  # argmax(-1) 的作用是沿着最后一个维度（通常是类别维度）取最大值对应的索引，即模型预测的类别
+    # p.label_ids 是真实的标签。
     return {"accuracy": accuracy_score(p.label_ids, preds)}
 
 
-# 在测试集上评估模型
+# 在测试集上评估模型 自动调用compute_metrics
 trainer.evaluate(encoded_dataset['test'], metric_key_prefix="eval")
-#{'eval_loss': 0.2, 'eval_accuracy': 0.85}
-#eval_loss: 0.2：这是模型在测试集上的损失值。
-#损失值是一个衡量模型预测与实际标签之间差异的指标。
-#较低的损失值通常表示模型的预测更接近于真实标签。
 
-#eval_accuracy: 0.85：这是模型在测试集上的准确率。
+"""{'eval_loss': 0.2, 'eval_accuracy': 0.85}
+# eval_loss: 0.2：这是模型在测试集上的损失值。
+# 损失值是一个衡量模型预测与实际标签之间差异的指标。
+# 较低的损失值通常表示模型的预测更接近于真实标签。
+# eval_accuracy: 0.85：这是模型在测试集上的准确率。
 # 准确率是指模型正确预测的样本数量占总样本数量的比例。
 # 在这个例子中，准确率为 0.85，意味着模型在测试集上有 85% 的样本被正确分类。
-
-# 步骤 7：导出模型
+"""
+# 7：导出模型
 # 保存模型和分词器
 model.save_pretrained('./sentiment_model')
 tokenizer.save_pretrained('./sentiment_model')
