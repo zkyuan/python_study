24ee44a201034709aa424211aa6dafd7acb97bce
zkyuan <2584278161@qq.com>
2025-03-06 16:18:35 +0800

    ...

--- py6_juke/day5-demo/prompt_templates/chat_prompt_templates.py
+++ py6_juke/day5-demo/prompt_templates/chat_prompt_templates.py
@@ -1,8 +1,9 @@
 from langchain_core.prompts import ChatPromptTemplate
+
 prompt_template = ChatPromptTemplate.from_messages([
     ("system", "You are a helpful assistant"),
-    ("user", "Tell me a joke about {topic}")
+    ("user", "Tell me a joke about {topic},{param}")
 ])
-result = prompt_template.invoke({"topic": "cats"})
+result = prompt_template.invoke({"topic": "cats", "param": "dogs"})
 print(result)
-#messages=[SystemMessage(content='You are a helpful assistant'), HumanMessage(content='Tell me a joke about cats')]
+# messages=[SystemMessage(content='You are a helpful assistant'), HumanMessage(content='Tell me a joke about cats')]
